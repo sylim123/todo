c9cd79889829f1ea74cb5ab6971039d005e2e910
seyoon <sylim123@naver.com>
2019-05-14 22:25:28 +0900

    first commit

---
+++ todo/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    checked = models.BooleanField()
+    created_data = models.DateTimeField(default=timezone.now)
+    published_data = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_data = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
